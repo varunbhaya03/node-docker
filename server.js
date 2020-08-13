4c09ae01d1fccc5f5caf7b01592b5ba5062ae831
varunbhaya03 <varun.bhaya03@gmail.com>
2020-08-13 16:37:26 +0530

    update

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const server = http.createServer((req, res) => {
   res.end('hello world from docker')
 })
 
-server.listen(8000, () => console.log(`Server running at 8000!`))
+server.listen(8000, () => console.log(`Server running at 8000!!`))
