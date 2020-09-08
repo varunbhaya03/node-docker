5d20824196d50b0df7712fe20c998e9f26fcc614
varunbhaya03 <varun.bhaya03@gmail.com>
2020-09-08 15:59:28 +0530

    update yml

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
-  res.end('Server started!')
+  res.end('Server tested and pushed to docker successfully!!')
 })
 server.listen(8080, () => console.log('Server running at 8080!'))
